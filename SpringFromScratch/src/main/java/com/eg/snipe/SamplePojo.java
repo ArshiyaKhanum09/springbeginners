f08183cb0de1f0aedb154ddf6e67912b78a86a83
ArshiyaKhanum09 <104981814+ArshiyaKhanum09@users.noreply.github.com>
2022-05-07 12:37:51 +0530

    Add files via upload

---
+++ SpringFromScratch/src/main/java/com/eg/snipe/SamplePojo.java
@@ -0,0 +1,19 @@
+package com.eg.snipe;
+
+public class SamplePojo { //pojo plain old java object
+
+	public SamplePojo() {
+		System.out.println("this is a constructor called");
+	}
+	
+	public void sending()
+	{
+		System.out.println("this is sending mail");
+	}
+	
+	public void receiving()
+	{
+		System.out.println("this is receiving mail");
+	}
+	
+}
